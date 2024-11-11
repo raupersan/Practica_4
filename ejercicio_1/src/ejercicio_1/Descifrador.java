b81f988ee8730a9299b770d071057bfa344c3db9
raupersan <raulps1798@gmail.com>
2024-11-11 10:29:50 +0100

    clases creadas

---
+++ ejercicio_1/src/ejercicio_1/Descifrador.java
@@ -0,0 +1,14 @@
+package ejercicio_1;
+
+import java.util.Scanner;
+
+public class Descifrador {
+
+	Scanner sc = new Scanner(System.in);
+
+	
+	public static void main(String[] args) {
+	final int longitud= 4;
+	}
+
+}
---
+++ ejercicio_1/src/ejercicio_1/DescifradorParalelo.java
@@ -0,0 +1,5 @@
+package ejercicio_1;
+
+public class DescifradorParalelo {
+
+}
