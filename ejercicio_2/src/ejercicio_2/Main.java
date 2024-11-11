014c3440a90b9e49afcc10210fc3a1dde2c188b6
unknown <raupersan>
2024-11-11 20:01:53 +0100

    funcion iniciar tablero por definir

--- ejercicio_2/src/ejercicio_2/Main.java
+++ ejercicio_2/src/ejercicio_2/Main.java
@@ -1,11 +1,22 @@
 package ejercicio_2;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		HashMap tablero = new HashMap(15,15);
+	
+	static Scanner sc = new Scanner(System.in);
+	private static void iniciarTablero(HashMap tablero) {
+		
 	}
+	public static void main(String[] args) {
+		HashMap tablero;
+		int nJugadores;
+		System.out.println("Introduce el número de jugadores");
+		nJugadores= sc.nextInt();
+		iniciarTablero(tablero, nJugadores);
+	}
+
+	
 
 }
