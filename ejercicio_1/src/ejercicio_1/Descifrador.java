7f5e2c6d64b7d49ef22c61e377062c1f4043a3b0
raupersan <raulps1798@gmail.com>
2024-11-11 11:28:32 +0100

    clases

--- ejercicio_1/src/ejercicio_1/Descifrador.java
+++ ejercicio_1/src/ejercicio_1/Descifrador.java
@@ -1,14 +1,34 @@
 package ejercicio_1;
 
+import java.util.HashMap;
 import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Descifrador {
 
-	Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
 
-	
-	public static void main(String[] args) {
-	final int longitud= 4;
+	public static byte[] getHash(String text) {
+		byte[] encodedhash = null;
+		MessageDigest digest;
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+			encodedhash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+		} catch (NoSuchAlgorithmException e) {
+			//
+			e.printStackTrace();
+		}
+		return encodedhash;
 	}
 
+	public static void main(String[] args) {
+		final int longitud = 4;
+		String contrasena;
+		byte[] hash;
+		System.out.println("Introduce tu contraseña de " + longitud + " dígitos");
+		contrasena = sc.nextLine();
+		hash = getHash(contrasena);
+	}
 }
