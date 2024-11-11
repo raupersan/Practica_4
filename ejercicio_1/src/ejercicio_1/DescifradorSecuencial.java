e24dd418ac4694c5b50756313b8f6cb3ac03f87e
Jorge <jpenajim@gmail.com>
2024-11-11 14:20:13 +0100

    secuencial

--- ejercicio_1/src/ejercicio_1/DescifradorSecuencial.java
+++ ejercicio_1/src/ejercicio_1/DescifradorSecuencial.java
@@ -6,59 +6,45 @@ import java.util.Scanner;
 
 public class DescifradorSecuencial {
 
-	public long descifrar(String contraseña){
+	public long descifrar(int ini, int longitud, byte[] contraseña) {
 		// TODO Auto-generated method stub
 
 		Scanner teclado = new Scanner(System.in);
-		//Random ran = new Random();
-		//String contraseña;
-		boolean encontrado= false;
+		boolean encontrado = false;
 
+		char c = (char) ini;
 		final String cadena = "abcdefghijklmnñopqrstuvwxyz";
-
-		System.out.println("Meta su contraseña");
-		contraseña = teclado.nextLine();
 
 		String aleatorio = "";
 		int contador = 0;
-		
+
 		long hora;
-		long tiempoFinal=0;
+		long tiempoFinal = 0;
 		do {
 			hora = System.currentTimeMillis();
+
+			for (int j = 0; j <longitud; j++) {
+				
 			
-			for (int i = 0; i < cadena.length(); i++) {
-				for (int a = 0; a < cadena.length(); a++) {
-					for (int h = 0; h < cadena.length(); h++) {
-						for (int j = 0; j < cadena.length(); j++) {
-							
-							aleatorio = "" + cadena.charAt(i) + cadena.charAt(a) + cadena.charAt(h) + cadena.charAt(j);
-							System.out.println("aleatorio: " + aleatorio.toString() + " " + "contraseña: " + contraseña);
+			for (char i = 'a'; i <= 'z'; i++) {
 
-							contador++;
-							if(aleatorio.equals(contraseña)) {
-								tiempoFinal= System.currentTimeMillis()-hora;
-								encontrado=true;
-							}
-							if(encontrado==true)
-								j=cadena.length();
+				aleatorio = aleatorio + i;
+				System.out.println("aleatorio: " + aleatorio.toString() + " " + "contraseña: " + contraseña);
 
-						}
-						if(encontrado==true)
-							h=cadena.length();
-					}
-					if(encontrado==true)
-						a=cadena.length();
+				contador++;
+				if (aleatorio.equals(contraseña)) {
+					tiempoFinal = System.currentTimeMillis() - hora;
+					encontrado = true;
 				}
+				if (encontrado == true)
+					i = 'z';
 
-				if(encontrado==true)
-					i=cadena.length();
-
+			}
+			//aleatorio = aleatorio + cadena.charAt(i);
 			}
 			System.out.println("aleatorio: " + aleatorio + " " + "contraseña: " + contraseña);
 		} while (!aleatorio.equals(contraseña));
 		System.out.println("El numero de intentos ha sido de: " + contador);
-		//System.out.println("Encontrado en " + tiempoFinal + " ms");
 		teclado.close();
 		return tiempoFinal;
 	}
