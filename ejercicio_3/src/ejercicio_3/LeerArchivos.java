67e3a397e4cfa4e53b2d0db0688fe13172753df1
raupersan <raulps1798@gmail.com>
2024-11-25 13:27:38 +0100

    LeerArchivos

---
+++ ejercicio_3/src/ejercicio_3/LeerArchivos.java
@@ -0,0 +1,33 @@
+package ejercicio_3;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+
+public class LeerArchivos {
+	Gson gson = new Gson();
+
+	public ArrayList<Cliente> leerClientes() throws IOException {
+		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+		for (int i = 1; i <= 6; i++) {
+			String ruta = "./data/Cliente" + i + ".json";
+			FileReader fr = new FileReader(ruta);
+			Cliente cliente = gson.fromJson(fr, Cliente.class);
+			clientes.add(cliente);
+
+		}
+		return clientes;
+	}
+
+	public Transferencia[] leerTransferencias() throws IOException {
+		Transferencia[] transferencias = null;
+		for (int i = 1; i <= 10; i++) {
+			String ruta = "./data/Transferencia" + i + ".json";
+			FileReader fr = new FileReader(ruta);
+			transferencias = gson.fromJson(fr, Transferencia[].class);
+		}
+		return transferencias;
+	}
+}
