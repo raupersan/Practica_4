e591095652747679af89903123b6c09338c40c75
raupersan <raulps1798@gmail.com>
2024-11-25 13:22:47 +0100

    funciones de leer archivos acabadas

--- ejercicio_3/src/ejercicio_3/LeerArchivos.java
+++ ejercicio_3/src/ejercicio_3/LeerArchivos.java
@@ -9,29 +9,24 @@ import com.google.gson.Gson;
 public class LeerArchivos {
 	Gson gson = new Gson();
 
-	public LeerArchivos() {
-
-	}
-
 	public ArrayList<Cliente> leerClientes() throws IOException {
 		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
 		for (int i = 1; i <= 6; i++) {
-				String ruta = "Cliente" + i+".json";
-				FileReader fr = new FileReader("datos.json");
-				Cliente cliente = gson.fromJson(fr, Cliente.class);
-				clientes.add(cliente);
-		
+			String ruta = "./data/Cliente" + i + ".json";
+			FileReader fr = new FileReader(ruta);
+			Cliente cliente = gson.fromJson(fr, Cliente.class);
+			clientes.add(cliente);
+
 		}
 		return clientes;
 	}
-	public ArrayList<Transferencia> leerTransferencias() throws IOException{
-		ArrayList<Transferencia> transferencias = new ArrayList<Transferencia>();
+
+	public Transferencia[] leerTransferencias() throws IOException {
+		Transferencia[] transferencias = null;
 		for (int i = 1; i <= 10; i++) {
-			String ruta = "Cliente" + i+".json";
-			FileReader fr = new FileReader("datos.json");
-			Transferencia transferencia = gson.fromJson(fr, Transferencia.class);
-			transferencias.add(transferencia);
-				
+			String ruta = "./data/Transferencia" + i + ".json";
+			FileReader fr = new FileReader(ruta);
+			transferencias = gson.fromJson(fr, Transferencia[].class);
 		}
 		return transferencias;
 	}
