dc351661f380b5b5afcb36373fc86d437921fbc4
raupersan <raulps1798@gmail.com>
2024-11-25 11:14:38 +0100

    lectura de Json hecha

--- ejercicio_3/src/ejercicio_3/LeerArchivos.java
+++ ejercicio_3/src/ejercicio_3/LeerArchivos.java
@@ -1,13 +1,38 @@
 package ejercicio_3;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import com.google.gson.Gson;
 
 public class LeerArchivos {
+	Gson gson = new Gson();
 
 	public LeerArchivos() {
-		
-	}
-    try {
 
-    }
-}
+	}
+
+	public ArrayList<Cliente> leerClientes() throws IOException {
+		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+		for (int i = 1; i <= 6; i++) {
+				String ruta = "Cliente" + i+".json";
+				FileReader fr = new FileReader("datos.json");
+				Cliente cliente = gson.fromJson(fr, Cliente.class);
+				clientes.add(cliente);
+		
+		}
+		return clientes;
+	}
+	public ArrayList<Transferencia> leerTransferencias() throws IOException{
+		ArrayList<Transferencia> transferencias = new ArrayList<Transferencia>();
+		for (int i = 1; i <= 10; i++) {
+			String ruta = "Cliente" + i+".json";
+			FileReader fr = new FileReader("datos.json");
+			Transferencia transferencia = gson.fromJson(fr, Transferencia.class);
+			transferencias.add(transferencia);
+				
+		}
+		return transferencias;
+	}
 }
