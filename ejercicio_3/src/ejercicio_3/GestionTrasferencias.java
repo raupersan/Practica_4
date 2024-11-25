d59b95566992e6b0294dcd1597a3875019342f6e
Jorge Peña <jpenajim@gmail.com>
2024-11-25 20:10:22 +0100

    Por fin funciona y javadoc

--- ejercicio_3/src/ejercicio_3/GestionTrasferencias.java
+++ ejercicio_3/src/ejercicio_3/GestionTrasferencias.java
@@ -2,19 +2,40 @@ package ejercicio_3;
 
 import java.util.concurrent.atomic.DoubleAdder;
 import java.util.ArrayList;
-import java.io.*;
 
-
+/**
+ * Clase que representa la gestión de transferencias entre clientes.
+ * Cada instancia de esta clase es un hilo que realiza una transferencia segura 
+ * entre dos clientes dentro de una lista, utilizando sincronización para 
+ * evitar condiciones de carrera.
+ */
 public class GestionTrasferencias extends Thread {
 
     private final ArrayList<Cliente> listaCliente;
     private final Transferencia transferencia;
 
+    /**
+     * Constructor de la clase GestionTrasferencias.
+     * 
+     * @param listaCliente   Lista de clientes involucrados en la transferencia.
+     * @param transferencia  Objeto que contiene los datos de la transferencia a realizar.
+     */
+    public GestionTrasferencias(ArrayList<Cliente> listaCliente, Transferencia transferencia) {
+        this.listaCliente = listaCliente;
+        this.transferencia = transferencia;
+    }
+
+    /**
+     * Método principal que gestiona la transferencia.
+     * 
+     * Encuentra los clientes de origen y destino en la lista, los bloquea de forma ordenada 
+     * para evitar bloqueos mutuos (deadlocks), y realiza la transferencia de forma segura.
+     */
     public void gestionTransferencias() {
         Cliente origen = null;
         Cliente destino = null;
 
-        // Encontrar el cliente origen y destino en la lista
+        // Buscar clientes origen y destino
         for (Cliente cliente : listaCliente) {
             if (transferencia.getOrigen().equals(cliente.getId())) {
                 origen = cliente;
@@ -24,6 +45,7 @@ public class GestionTrasferencias extends Thread {
             }
         }
 
+        // Determinar el orden de bloqueo para evitar deadlocks
         Object primero, segundo;
         if (System.identityHashCode(origen) < System.identityHashCode(destino)) {
             primero = origen;
@@ -33,35 +55,32 @@ public class GestionTrasferencias extends Thread {
             segundo = origen;
         }
 
+        // Sincronizar en el orden determinado
         synchronized (primero) {
             System.out.println(Thread.currentThread().getName() + " bloqueó " + origen.getId());
             try {
-                Thread.sleep(100);
+                Thread.sleep(100); // Simula el tiempo de procesamiento
             } catch (InterruptedException e) {
-               e.printStackTrace();
-
+                e.printStackTrace();
             }
             synchronized (segundo) {
                 System.out.println(Thread.currentThread().getName() + " bloqueó " + destino.getId());
+
                 // Realizar la transferencia
                 origen.restarSaldo(transferencia.getMonto());
                 destino.agregarSaldo(transferencia.getMonto());
-                System.out.println("Transferencia completada de " + origen.getId() + " a " + destino.getId() + 
+                System.out.println("Transferencia completada de " + origen.getId() + " a " + destino.getId() +
                                    " por monto: " + transferencia.getMonto());
             }
         }
     }
 
-	public GestionTrasferencias(ArrayList<Cliente> listaCliente, Transferencia transferencia) {
-		this.listaCliente = listaCliente;
-		this.transferencia = transferencia;
-
-	}
-
-	@Override
-	public void run() {
-		this.gestionTransferencias();
-
-	}
-
+    /**
+     * Sobrescribe el método run() para iniciar el proceso de transferencia 
+     * cuando el hilo sea ejecutado.
+     */
+    @Override
+    public void run() {
+        this.gestionTransferencias();
+    }
 }
